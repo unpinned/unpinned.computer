5275811f60df566efddc430c62ff3e403cd7081f
unpinned <furkanyilmaz@skiff.com>
2023-03-01 14:08:02 +0300

    make magic happen only on hover

--- static/magicy.js
+++ static/magicy.js
@@ -13,10 +13,27 @@ const animate = star => {
     star.style.animation = "";
 }
 
-for (const star of document.getElementsByClassName("magic-star")) {
-    setTimeout(() => {
-        animate(star);
+let timeouts = [],
+    intervals = [];
 
-        setInterval(() => animate(star), 1000);
-    }, index++ * (interval / 3))
+const magic = document.querySelector(".magic");
+
+magic.onmouseenter = () => {
+    let index = 1;
+
+    for (const star of document.getElementsByClassName("magic-star")) {
+        timeouts.push(setTimeout(() => {
+            animate(star);
+
+            intervals.push(setInterval(() => animate(star), 1000));
+        }, index++ * 300));
+    };
+}
+
+magic.onmouseleave = onMouseLeave = () => {
+    for (const t of timeouts) clearTimeout(t);
+    for (const i of intervals) clearInterval(i);
+
+    timeouts = [];
+    intervals = [];
 }
